995859744105d7c335ce879d14a40be08ea7bc69
Alex Dima <alexdima@microsoft.com>
2016-08-19 19:32:40 +0200

    show time in logger

--- src/logger.ts
+++ src/logger.ts
@@ -13,20 +13,53 @@ interface IWriteFunc {
 	(what:string): void;
 }
 
+function twoDigits(n:number): string {
+	if (n < 10) {
+		return '0' + n;
+	}
+	return String(n);
+}
+function threeDigits(n:number): string {
+	if (n < 10) {
+		return '00' + n;
+	}
+	if (n < 100) {
+		return '0' + n;
+	}
+	return String(n);
+}
+
+function time(): string {
+	let now = new Date();
+	let h = now.getHours();
+	let m = now.getMinutes();
+	let s = now.getSeconds();
+	let ms = now.getMilliseconds();
+	return `[${twoDigits(h)}:${twoDigits(m)}:${twoDigits(s)}.${threeDigits(ms)}]`
+}
+
 function bind(prefix:string): IWriteFunc {
-	return (what:string) => outputChannel.appendLine(prefix + what);
+	return (what:string) => outputChannel.appendLine(time() + prefix + what);
+}
+
+function rpad(str:string, n:number): string {
+	while (str.length < n) {
+		str = str + ' ';
+	}
+	return str;
 }
 
 export const log = {
 	error: bind('[ERROR]: '),
-	warn: bind('[WARN]: '),
-	info: bind('[INFO]: '),
+	warn: bind('[WARN ]: '),
+	info: bind('[INFO ]: '),
 	debug: bind('[DEBUG]: '),
 	bind: (prefix:string) => {
+		prefix = rpad(prefix, 15);
 		return {
 			error: bind('[ERROR][' + prefix + ']: '),
-			warn: bind('[WARN][' + prefix + ']: '),
-			info: bind('[INFO][' + prefix + ']: '),
+			warn: bind('[WARN ][' + prefix + ']: '),
+			info: bind('[INFO ][' + prefix + ']: '),
 			debug: bind('[DEBUG][' + prefix + ']: '),
 		}
 	}
