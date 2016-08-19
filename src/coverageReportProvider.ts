b5c61686d38a531fbcbac620a9ce4419e5e68496
Alex Dima <alexdima@microsoft.com>
2016-08-19 19:54:00 +0200

    Extract CoverageReportProvider

---
+++ src/coverageReportProvider.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+import {DataBank} from './dataBank';
+
+export class CoverageReportProvider implements vscode.TextDocumentContentProvider {
+
+	public static SCHEME = 'lcov';
+	public static COVERAGE_REPORT_URI = vscode.Uri.parse('lcov:coverage-report');
+
+	private static COVERAGE_REPORT_TEMPLATE: string;
+	public static init(ctx:vscode.ExtensionContext): void {
+		this.COVERAGE_REPORT_TEMPLATE = fs.readFileSync(ctx.asAbsolutePath('./resources/coverage-report.html')).toString();
+	}
+
+	private _dataBank:DataBank;
+
+	private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+	public onDidChange = this._onDidChange.event;
+
+	constructor(dataBank:DataBank) {
+		this._dataBank = dataBank;
+		this._dataBank.onDidChange(() => {
+			this._onDidChange.fire(CoverageReportProvider.COVERAGE_REPORT_URI);
+		});
+	}
+
+	public provideTextDocumentContent(uri: vscode.Uri): string {
+		let workspace = vscode.workspace.rootPath;
+
+		let data = this._dataBank.getSummary();
+		data.forEach((entry) => {
+			(<any>entry).path = entry.absolutePath.substr(workspace.length + 1);
+		});
+
+		return (
+			CoverageReportProvider.COVERAGE_REPORT_TEMPLATE
+			.replace(/\/\*\$data\*\//, JSON.stringify(data))
+			// .replace(/\/\*\$workspace\*\//, '"' + vscode.workspace.rootPath.replace(/\\/g, '\\\\') + '"')
+		);
+	}
+}
--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,7 @@ import {Configuration} from './configuration';
 import {IRawCoverageData, IRawBranchDetail} from './loader';
 import {loadMany} from './loader';
 import {DataBank} from './dataBank';
+import {CoverageReportProvider} from './coverageReportProvider';
 
 abstract class QuickPickItem implements vscode.QuickPickItem {
 
@@ -66,43 +67,7 @@ class StopSourceFileWatcher extends QuickPickItem {
 	}
 }
 
-class CoverageReportProvider implements vscode.TextDocumentContentProvider {
 
-	public static SCHEME = 'lcov';
-	public static COVERAGE_REPORT_URI = vscode.Uri.parse('lcov:coverage-report');
-
-	private static COVERAGE_REPORT_TEMPLATE: string;
-	public static init(ctx:vscode.ExtensionContext): void {
-		this.COVERAGE_REPORT_TEMPLATE = fs.readFileSync(ctx.asAbsolutePath('./resources/coverage-report.html')).toString();
-	}
-
-	private _dataBank:DataBank;
-
-	private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
-	public onDidChange = this._onDidChange.event;
-
-	constructor(dataBank:DataBank) {
-		this._dataBank = dataBank;
-		this._dataBank.onDidChange(() => {
-			this._onDidChange.fire(CoverageReportProvider.COVERAGE_REPORT_URI);
-		});
-	}
-
-	public provideTextDocumentContent(uri: vscode.Uri): string {
-		let workspace = vscode.workspace.rootPath;
-
-		let data = this._dataBank.getSummary();
-		data.forEach((entry) => {
-			(<any>entry).path = entry.absolutePath.substr(workspace.length + 1);
-		});
-
-		return (
-			CoverageReportProvider.COVERAGE_REPORT_TEMPLATE
-			.replace(/\/\*\$data\*\//, JSON.stringify(data))
-			// .replace(/\/\*\$workspace\*\//, '"' + vscode.workspace.rootPath.replace(/\\/g, '\\\\') + '"')
-		);
-	}
-}
 
 
 
